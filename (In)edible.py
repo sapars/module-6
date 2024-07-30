ba0cecd44523856ff64a3b5e08c9472e72ce1ecf
sapars <110113627+sapars@users.noreply.github.com>
2024-07-30 15:52:54 +0300

    Update (In)edible.py
    добавлена обратная связь в методе eat
    Добавлены проверки

--- (In)edible.py
+++ (In)edible.py
@@ -12,9 +12,11 @@ class Animal:
 
     def eat(self, food: Plant):
         if food.edible:
+            print(f'{self.name} съел {food.name}')
             self.fed = True
             self.alive = True
         else:
+            print(f'{self.name} не стал есть {food.name}')
             self.fed = False
             self.alive = False
 
@@ -36,11 +38,22 @@ class Fruit(Plant):
     def __init__(self, name, edible = True):
         super().__init__(name, edible)
 
-m = Mammal("Lion")
-p = Predator("Wolf")
-c = Fruit('cherry')
-t= Flower()
 
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
 p.eat(t)
 
 print(p.fed, p.name, t.name)
